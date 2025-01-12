d5f86a9f494830286db94eb14c3f919c00d24a7d
deuk94 <knli0025@gmail.com>
2025-01-12 15:03:44 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        
+        String str = myString.replace("A", "X").replace("B", "A").replace("X", "B");
+        
+        if (str.contains(pat)) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
