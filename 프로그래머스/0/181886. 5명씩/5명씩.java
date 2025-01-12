ce7f35aaaac44bc01c70e7f87c99ae3f2f519bd2
deuk94 <knli0025@gmail.com>
2025-01-12 18:30:08 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -3,9 +3,8 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] names) {
         List<String> nameList = new ArrayList<>();
-        nameList.add(names[0]);
         
-        for (int i = 1; i < names.length; i++) {
+        for (int i = 0; i < names.length; i++) {
             if (i % 5 == 0) {
                 nameList.add(names[i]);
             }
