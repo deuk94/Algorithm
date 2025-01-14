ae03e763d810f77bd27d322f3faaa80ce4ba668d
deuk94 <knli0025@gmail.com>
2025-01-14 10:08:50 +0900

    [level 0] Title: l로 만들기, Time: 1203.99 ms, Memory: 394 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,6 +1,14 @@
 class Solution {
     public String solution(String myString) {
-        String answer = myString.replaceAll("[a-k]", "l");
+        String answer = "";
+        
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) < 'l') {
+                answer += "l";
+            } else {
+                answer += String.valueOf(myString.charAt(i));
+            }
+        }
         return answer;
     }
 }
