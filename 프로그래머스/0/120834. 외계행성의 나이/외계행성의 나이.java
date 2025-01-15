1c842ff81d25acb8686207acc144b318a3d9bc4a
deuk94 <knli0025@gmail.com>
2025-01-15 09:41:56 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.20 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int age) {
+        String answer = Integer.toString(age);
+        
+        answer = answer.replaceAll("0", "a")
+                        .replaceAll("1", "b")
+                        .replaceAll("2", "c")
+                        .replaceAll("3", "d")
+                        .replaceAll("4", "e")
+                        .replaceAll("5", "f")
+                        .replaceAll("6", "g")
+                        .replaceAll("7", "h")
+                        .replaceAll("8", "i")
+                        .replaceAll("9", "j");
+        return answer;
+    }
+}
