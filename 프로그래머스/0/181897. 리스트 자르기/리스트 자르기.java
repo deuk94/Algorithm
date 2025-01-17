35a252d6c7ac6e4940c3aaf9d1579f8d92efbd57
deuk94 <knli0025@gmail.com>
2025-01-17 21:26:26 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> arr = new ArrayList<>();
+        
+        if (n == 1) {
+            for (int i = 0; i <= slicer[1]; i++) {
+                arr.add(num_list[i]);
+            }
+        } else if (n == 2) {
+            for (int i = slicer[0]; i < num_list.length; i++) {
+                arr.add(num_list[i]);
+            }
+        } else if (n == 3) {
+            for (int i = slicer[0]; i <= slicer[1]; i++) {
+                arr.add(num_list[i]);
+            }
+        } else {
+            for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                arr.add(num_list[i]);
+            }
+        }
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
