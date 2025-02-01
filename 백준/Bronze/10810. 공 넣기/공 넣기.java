a5c28d0959a28b92c7d4575ea0ac53cfe9a575a4
deuk94 <knli0025@gmail.com>
2025-02-01 09:06:36 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] intArr = new int[n];
+
+        for (int a = 0; a < m; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int b = i - 1; b < j; b++) {
+                intArr[b] = k;
+            }
+        }
+
+        for (int i = 0; i < intArr.length; i++) {
+            bw.write(intArr[i] + " ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
