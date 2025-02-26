0f26717b1e36dab06bdfb5b0ae515fbf7ac5f82c
deuk94 <knli0025@gmail.com>
2025-02-26 11:42:31 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16516 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (arr[i] + arr[j] < m) {
+                i++;
+            } else if (arr[i] + arr[j] > m) {
+                j--;
+            } else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+
+        bw.write(count + "");
+        br.close();
+        bw.close();
+    }
+}
