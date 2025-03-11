d57c9b47ac2bbdbcf99dab2bb63a860328e61e7c
deuk94 <knli0025@gmail.com>
2025-03-11 10:11:59 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] arr = br.readLine().split("");
+        int[] intArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            intArr[i] = Integer.parseInt(arr[i]);
+        }
+
+        for (int i = 0; i < intArr.length - 1; i++) {
+            for (int j = i + 1; j < intArr.length; j++) {
+                if (intArr[i] < intArr[j]) {
+                    int temp = intArr[i];
+                    intArr[i] = intArr[j];
+                    intArr[j] = temp;
+                }
+            }
+        }
+
+        for (int i : intArr) {
+            bw.write(i + "");
+        }
+        br.close();
+        bw.close();
+    }
+}
