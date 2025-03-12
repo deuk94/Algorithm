a805f10a51e4c88fd399be433f4773dbd1d306ca
deuk94 <knli0025@gmail.com>
2025-03-12 15:13:28 +0900

    [Bronze III] Title: 세 수, Time: 120 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] arr = {a, b, c};
+        Arrays.sort(arr);
+
+        bw.write(arr[1] + "\n");
+        br.close();
+        bw.close();
+    }
+}
